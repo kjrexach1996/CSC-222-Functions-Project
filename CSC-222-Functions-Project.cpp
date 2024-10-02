566d60d7a0e2109872adeacde99ce6223f0d7c79
Kevin Rexach <kaiorex@hotmail.com>
2024-10-02 15:03:36 -0400

    Final Project done, must had comments along with pre and postconditions for user defined functions.

--- CSC-222-Functions-Project.cpp
+++ CSC-222-Functions-Project.cpp
@@ -8,12 +8,14 @@ using namespace std;
 
 double calcDistance(double x1, double y1, double x2, double y2);
 double calcRadius(double x1, double y1, double x2, double y2);
+double calcCircumference(double radius);
+double calcArea(double radius);
 
 
 int main()
 {
     double x1, y1, x2, y2, radius, circumference, area;
-    bool correct = false;
+    bool correct;
     char confirm = 'n', answer;
 
     cout << "Welcome! This program returns various properties of a circle.\n";
@@ -33,6 +35,8 @@ int main()
 
         cout << "Center point coordinates: (" << x1 << ", " << y1 << ")\n";
         cout << "Circumference point coordinates: (" << x2 << ", " << y2 << ")\n";
+
+        correct = false;
      
         while (!correct)
         {
@@ -41,7 +45,7 @@ int main()
             if (answer == 'y' || answer == 'n')
             {
                 confirm = answer;
-                correct == true;
+                correct = true;
             }
             else
                 cout << "Invalid input!\n";
@@ -50,9 +54,12 @@ int main()
         
     }
     radius = calcRadius(x1, y1, x2, y2);
+    circumference = calcCircumference(radius);
+    area = calcArea(radius);
 
-    cout << setprecision(2);
-    cout << "The radius of the circle is " << radius << "units.";
+    cout << setprecision(2) << fixed << "The radius of the circle is " << radius << " units.\n";
+    cout << setprecision(2) << fixed << "The circumference of the circle is " << circumference << " units.\n";
+    cout << setprecision(2) << fixed << "The area of the circle is " << area << " square units.\n";
     return 0;
 }
 
@@ -64,4 +71,14 @@ double calcRadius(double x1, double y1, double x2, double y2)
 double calcDistance(double x1, double y1, double x2, double y2)
 {
     return sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
+}
+
+double calcCircumference(double radius)
+{
+    return 2 * 3.1416 * radius;
+}
+
+double calcArea(double radius)
+{
+    return 3.1416 * pow(radius, 2);
 }
